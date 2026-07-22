4bf47b0fae5a963eb8f825d994864c07c41dc5f7
dskim <dskim@assesta.com>
2026-07-22 10:10:22 +0900

    fix: disable landing popup link

--- lib/landing-popup-config.ts
+++ lib/landing-popup-config.ts
@@ -11,7 +11,7 @@ export const LANDING_POPUPS: LandingPopupItem[] = [
   {
     id: "2026-holiday05",
     imageUrl: "https://img.assesta.com/popup/2026_notice_06.jpg",
-    linkUrl: "https://www.career4u.net/Board/Board_View.asp?Seq=16807&nowPage=1&Board_Cd=A051&Board_Sub_Cd=0&fb_t=%BC%AD%B9%F6%B1%B3%C3%BC+%BE%C8%B3%BB",
+    linkUrl: "#",
     alt: "서버교체안내",
     hideForHours: 24,
     enabled: true,
