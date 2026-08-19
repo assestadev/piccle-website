3d5d40fb8ea89f1a6cf9943d28d05c1bf7f23b21
dskim <dskim@assesta.com>
2026-08-19 10:31:33 +0900

    fix: disable august landing popup

--- lib/landing-popup-config.ts
+++ lib/landing-popup-config.ts
@@ -14,6 +14,6 @@ export const LANDING_POPUPS: LandingPopupItem[] = [
     linkUrl: "https://www.career4u.net/Board/Board_View.asp?Seq=16860&nowPage=1&Board_Cd=A051",
     alt: "8월 휴무안내",
     hideForHours: 24,
-    enabled: true,
+    enabled: false,
   },
 ]
